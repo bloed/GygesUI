828da376e813c9b6d73cff6bc0b35b2472d6456f
bloed <josuearrietasalas@gmail.com>
2015-11-02 15:12:17 -0600

    interfaz basica
    Le parece?

---
+++ Gyges.pl
@@ -0,0 +1,310 @@
+/*Instituto Tecnológico de Costa Rica
+;Escuela de Computación
+;Ingeniería en Computación
+;Lenguajes de Programacion
+;Grupo 2
+;Tarea: Gyges
+;Adrian Lopez Quesada
+;2014081634
+;Kirstein Gätjens S.
+;14/10/15
+
+;Manual de Usuario:
+  Solo tiene una relacion para el AI de Gyges, se utiliza 
+  Gyges(TableroOriginal,TableroFinal,PosisicionActual,PosicionFinal,Turno).
+
+  Para el tablero original se debe especificar todos los 36 campos del tablero a como estan en el juego,
+  un ejemplo serian los siguientes:
+
+  [f1,f2,f0,f1,f2,f1,f0,f0,f2,f0,f0,f0,f0,f0,f0,f0,f0,f0,f0,f0,f3,f0,f0,f0,f0,f0,f0,f0,f0,f0,f3,f3,f0,f1,f2,f1]
+
+  [f3,f2,f0,f1,f2,f3,f0,f0,f0,f2,f0,f0,f0,f0,f0,f0,f0,f0,f0,f0,f0,f3,f0,f0,f0,f0,f0,f0,f0,f0,f3,f3,f1,f0,f2,f1] 
+
+La variable solucion no se pone ningun valor, ya que aqui devuelve el tablero de solucion, la posicion actual y la final
+tampoco se administran por las mismas razones, el turno si de debe administrar para decidir en que parte del tablero se va a jugar
+en que parte se considera mas cercana al jugador. Se utiliza p1 para indicar turno del jugador superior (0) y p2 del inferior (37).
+
+El tablero tiene la siguientes posiciones:
+
+[1,2,3,4,5,6,
+7,8,9,10,11,12,
+13,14,15,16,17,18,
+19,20,21,22,23,24,
+25,26,27,28,29,30,
+31,32,33,34,35,36].
+
+Por lo que al regresar el resultado se utilizan estas posiciones, siendoe el 0 la posicion ganadora del p2 y 
+37 la posicion ganadora de p1.
+
+En caso de no haber jugada alguna para poder ganar entnces retorna falso, de lo contrario siempre devuelve una jugada
+
+
+;Analisis de Resultados: Toda la AI funciona
+Movimientos legales de cada jugador segun el turno: A
+Movimiento ganador: A
+Mejor movimiento para no darle el gane al enemigo: A
+Se realizan puentes en los mivimientos: A
+No se encicla el AI: A
+Colocar todas las piezas antes de iniciar el juego: F No se implemento del todo
+*/
+
+%--------------------Valores de las fichas----------------------------------
+fichaval(f0,0).
+fichaval(f1,1).
+fichaval(f2,2).
+fichaval(f3,3).
+
+%--------------------Relaciones dentro del tablero y en la misma fila----------------------------------
+
+dentroV(Nuevo):- Nuevo<37,Nuevo>0.
+
+
+dentroH(Nuevo,Viejo):- 0<Viejo,
+					  7>Viejo,
+					  0<Nuevo,
+					  7>Nuevo.
+dentroH(Nuevo,Viejo):- 6<Viejo,
+					  13>Viejo,
+					  6<Nuevo,
+					  13>Nuevo.
+dentroH(Nuevo,Viejo):- 12<Viejo,
+					  19>Viejo,
+					  12<Nuevo,
+					  19>Nuevo.
+dentroH(Nuevo,Viejo):- 18<Viejo,
+					  25>Viejo,
+					  18<Nuevo,
+					  25>Nuevo.
+dentroH(Nuevo,Viejo):- 24<Viejo,
+					  31>Viejo,
+					  24<Nuevo,
+					  31>Nuevo.
+dentroH(Nuevo,Viejo):- 30<Viejo,
+					  37>Viejo,
+					  30<Nuevo,
+					  37>Nuevo. 
+
+%--------------------Valores del tipo del arbol, valores para cortar profundidad del arbol----------------------------------
+recursion(recursion).
+find1(find1).
+
+%-------------------Complemento para la tabla inversa del p2----------------------------------
+
+complemento(Valor,Comp):- Comp is 37-Valor.
+
+%--------------------Relacion de no pasar por el mismo espacio----------------------------------
+
+nopasado(nopasado).
+
+tablemoves([nopasado,nopasado,nopasado,nopasado,nopasado,nopasado,
+		nopasado,nopasado,nopasado,nopasado,nopasado,nopasado,
+		nopasado,nopasado,nopasado,nopasado,nopasado,nopasado,
+		nopasado,nopasado,nopasado,nopasado,nopasado,nopasado,
+		nopasado,nopasado,nopasado,nopasado,nopasado,nopasado,
+		nopasado,nopasado,nopasado,nopasado,nopasado,nopasado]).
+
+%--------------------Relaciones de los jugadores----------------------------------
+
+player1(p1).
+player2(p2).
+
+%--------------------Relaciones de las filas ----------------------------------
+
+row1([T11,T12,T13,T14,T15,T16,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_],[T11, T12, T13, T14, T15, T16],1).
+row1([_,_,_,_,_,_,
+		T11,T12,T13,T14,T15,T16,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_],[T11, T12, T13, T14, T15, T16],7).
+row1([_,_,_,_,_,_,
+		_,_,_,_,_,_,
+		T11,T12,T13,T14,T15,T16,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_,
+		_,_,_,_,_,_],[T11, T12, T13, T14, T15, T16],13).
+
+rowValues([]):-fail.
+rowValues([f0|Tail]):-rowValues(Tail).
+rowValues([f1|_]).
+rowValues([f2|_]).
+rowValues([f3|_]).
+
+%--------------------Relaciones de victoria----------------------------------
+
+win(31).
+win(32).
+win(33).
+win(34).
+win(35).
+win(36).
+
+%--------------------posicion de una valor en el tablero----------------------------------
+
+tableval([Head|_],1,Head).
+tableval([_|Tail],Number,Val):- Number2 is Number - 1,
+								tableval(Tail,Number2,Val).
+
+
+%--------------------Cambio de valores en el tablero----------------------------------
+
+changeValues([_|Tail], 1, Val,[Val|Tail]).
+
+changeValues([Head|Tail],Number, Val, [Head|Tail2]):- Number2 is Number - 1,
+											changeValues(Tail,Number2, Val, Tail2).
+
+%--------------------Auxiliar para encontrar 1's y 0's del AI----------------------------------
+
+gygesAux(Values,Sol,PosActual,PosFinal,Row1,Number):-
+	tablemoves(Table),
+	gygesmove1(Row1,Number,Values,Table,Sol,PosFinal,PosActual,find1),!.
+
+gygesAux(Values,Sol,PosActual,PosFinal,Row1,Number):-
+	tablemoves(Table),
+	gygesmove1(Row1,Number,Values,Table,Sol,PosFinal,PosActual,recursion),!.
+
+
+gyges(Values,Sol,PosActual,PosFinal,Turn):-
+		player1(Turn),!,
+		row1(Values,Row1,Number),
+		rowValues(Row1),!,
+		gygesAux(Values,Sol,PosActualAux,PosFinalAux,Row1,Number),!, 
+		move(PosActualAux,PosActual), move(PosFinalAux,PosFinal),!.
+
+gyges(Values,Sol,PosActual,PosFinal,Turn):-
+		player2(Turn),!,
+		reverse(Values,RevVal),
+		row1(RevVal,Row1,Number),
+		rowValues(Row1),!,
+		gygesAux(RevVal,RevSol,RevPosActual,RevPosFinal,Row1,Number),!,
+		reverse(RevSol,Sol), complemento(RevPosActual,PosActualAux), complemento(RevPosFinal,PosFinalAux),!
+		,move(PosActualAux,PosActual), move(PosFinalAux,PosFinal),!.
+
+%--------------------Relacion de control del movimiento del contrario en mi turno----------------------------------
+
+gygesAdvAux(RevVal,Row1,Number):-
+	tablemoves(Table),
+	gygesmove1(Row1,Number,RevVal,Table,Sol,PosFinal,PosActual,find1),!,fail.
+
+gygesAdvAux(RevVal,Row1,Number):-!.
+
+gygesAdversary(Values):-
+		reverse(Values,RevVal),
+		row1(RevVal,Row1,Number),
+		rowValues(Row1),!,
+		gygesAdvAux(RevVal,Row1,Number). 
+
+
+gygesAdversary(_,_,0).
+
+%--------------------Relacion que encuentra una posicion y valores en una fila, llama a todas ls jugadas de esa fila----------------------------------
+
+gygesmove1([],_,_,_,_,_,_,_):- !,fail.
+
+gygesmove1([f0|Tail],Number,Values,Table,Sol,PosFinal,PosInicial,Type):- Number2 is Number + 1, 								
+								gygesmove1(Tail,Number2,Values,Table,Sol,PosFinal,PosInicial,Type).
+								
+									
+gygesmove1([f1|Tail],Number,Values,Table,Sol,PosFinal,PosInicial,Type):- gygesplay(Values,Number,Table, 1, Sol,f1,Type,f0,PosFinal), PosInicial is Number,!.
+gygesmove1([f1|Tail],Number,Values,Table,Sol,PosFinal ,PosInicial,Type):- Number2 is Number + 1, gygesmove1(Tail,Number2,Values,Table,Sol,PosFinal,PosInicial,Type).
+
+gygesmove1([f2|Tail],Number,Values,Table,Sol,PosFinal,PosInicial,Type):- gygesplay(Values,Number,Table, 2, Sol,f2,Type,f0,PosFinal), PosInicial is Number,!.
+gygesmove1([f2|Tail],Number,Values,Table,Sol,PosFinal,PosInicial,Type):- Number2 is Number + 1, gygesmove1(Tail,Number2,Values,Table,Sol,PosFinal,PosInicial,Type).
+
+gygesmove1([f3|Tail],Number,Values,Table,Sol,PosFinal,PosInicial,Type):- gygesplay(Values,Number,Table, 3, Sol,f3,Type,f0,PosFinal), PosInicial is Number,!.
+gygesmove1([f3|Tail],Number,Values,Table,Sol,PosFinal,PosInicial,Type):- Number2 is Number + 1, gygesmove1(Tail,Number2,Values,Table,Sol,PosFinal,PosInicial,Type).
+
+
+%--------------------Relacion que se encarga de todas las extensiones del arbol de una sola ficha----------------------------------
+
+gygesplaychange(Values, PosActual, Moves, 1, WinnerTable, FindPos,InitVal,Type,SavedFicha,PosFinal):-
+			tableval(Values,FindPos,Val), fichaval(Val,NewMoves), tableval(Moves, FindPos, Occupied), nopasado(Occupied), changeValues(Values, PosActual, SavedFicha, Values2),
+			changeValues(Moves,PosActual,pasado,MovesFinal), gygesplay(Values2, FindPos, MovesFinal, NewMoves, WinnerTable,InitVal,Type,Val,PosFinal).
+
+gygesplaychange(Values, PosActual, Moves, 2, WinnerTable, FindPos,InitVal,Type,SavedFicha,PosFinal):-
+			tableval(Values,FindPos,Val), fichaval(Val,0), tableval(Moves, FindPos, Occupied), nopasado(Occupied), changeValues(Values, PosActual, SavedFicha, Values2),
+			NewCount is 1, changeValues(Values2,FindPos, InitVal,ValuesFinal),
+			changeValues(Moves,PosActual,pasado,MovesFinal), gygesplay(ValuesFinal, FindPos, MovesFinal, NewCount, WinnerTable,InitVal,Type,f0,PosFinal).
+
+gygesplaychange(Values, PosActual, Moves, 3, WinnerTable, FindPos,InitVal,Type,SavedFicha,PosFinal):-
+			tableval(Values,FindPos,Val), fichaval(Val,0), tableval(Moves, FindPos, Occupied), nopasado(Occupied), changeValues(Values, PosActual, SavedFicha, Values2),
+			NewCount is 2, changeValues(Values2,FindPos, InitVal,ValuesFinal),
+			changeValues(Moves,PosActual,pasado,MovesFinal), gygesplay(ValuesFinal, FindPos, MovesFinal, NewCount, WinnerTable,InitVal,Type,f0,PosFinal).  
+
+
+gygesplay(Values,PosActual,Moves,0,ValuesFin,InitVal,recursion,SavedFicha,PosActual):- changeValues(Values,PosActual,InitVal,ValuesFin), 
+gygesAdversary(ValuesFin),!.
+
+gygesplay(Values, PosActual, Moves, Count, Values,InitVal,find1,SavedFicha,37):- Count = 1, win(PosActual),!.
+
+
+%-----------------------------------------------------------------------------------------------------------------------------
+
+gygesplay(Values, PosActual, Moves, Count, WinnerTable,InitVal,Type,SavedFicha,PosFinal):-  FindPos is PosActual + 6,
+dentroV(FindPos),
+gygesplaychange(Values,PosActual,Moves,Count,WinnerTable,FindPos,InitVal,Type,SavedFicha,PosFinal).
+
+gygesplay(Values, PosActual, Moves, Count, WinnerTable,InitVal,Type,SavedFicha,PosFinal):-  FindPos is PosActual + 1,
+dentroH(FindPos,PosActual),
+gygesplaychange(Values,PosActual,Moves,Count,WinnerTable,FindPos,InitVal,Type,SavedFicha,PosFinal).
+
+
+gygesplay(Values, PosActual, Moves, Count, WinnerTable,InitVal,Type,SavedFicha,PosFinal):-  FindPos is PosActual - 1,
+dentroH(FindPos,PosActual),
+gygesplaychange(Values,PosActual,Moves,Count,WinnerTable,FindPos,InitVal,Type,SavedFicha,PosFinal).
+
+
+gygesplay(Values, PosActual, Moves, Count, WinnerTable,InitVal,Type,SavedFicha,PosFinal):-  FindPos is PosActual - 6,
+dentroV(FindPos),
+gygesplaychange(Values,PosActual,Moves,Count,WinnerTable,FindPos,InitVal,Type,SavedFicha,PosFinal).
+
+
+%Disculpe por el siguiente codigo, debido a la entrega de resumenes esta misma fecha no tuve tiempo suficiente 
+%para hacer esta parte correctamente...
+
+move(0,win).
+move(1,a0).
+move(2,a1).
+move(3,a2).
+move(4,a3).
+move(5,a4).
+move(6,a5).
+move(7,b0).
+move(8,b1).
+move(9,b2).
+move(10,b3).
+move(11,b4).
+move(12,b5).
+move(13,c0).
+move(14,c1).
+move(15,c2).
+move(16,c3).
+move(17,c4).
+move(18,c5).
+move(19,d0).
+move(20,d1).
+move(21,d2).
+move(22,d3).
+move(23,d4).
+move(24,d5).
+move(25,e0).
+move(26,e1).
+move(27,e2).
+move(28,e3).
+move(29,e4).
+move(30,e5).
+move(31,f0).
+move(32,f1).
+move(33,f2).
+move(34,f3).
+move(35,f4).
+move(36,f5).
+move(37,win).
+
+%----------------------------------------------------------------------------------------------------------------
+
--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include <stdlib.h>
+#include "SWI-Prolog.h"
 #include <gtk/gtk.h>
 
 
-GtkWidget *PantallaPrimerTurno;/*creamos primer pantalla, es global*/
+GtkWidget *mainWindow;/*creamos la pantalla global*/
+GtkWidget  *labelJugador;/*será el label que contiene el jugador actual*/
+/*gyges([f3,f2,f3,f0,f0,f0,f1,f0,f0,f0,f0,f0,f1,f0,f1,f3,f0,f0,f0,f2,f0,f0,f2,f3,f0,f0,f2,f0,f0,f1,f0,f0,f0,f0,f0,f0],X,Actual,Pasada, p2).*/
 
 int main (int argc, char *argv[])
 {
+    printf("Selecciona quien comienza: i (infierno) o c (cielo) \n");
+    char turnoInicial[1];
+    scanf("%s", &turnoInicial);
     iniciarGtk(argc,argv);
-    primerPantalla();
+    if(strcmp(turnoInicial,"i") ==0){
+        printf("juega infierno\n");
+        pantallaPrincipal(0);
+    }
+    else if(strcmp(turnoInicial,"c") ==0){
+        printf("juega cielo\n");
+        pantallaPrincipal(1);
+    }
+    else{
+        printf("Opcion incorrecta");
+    }
     return 0;
 }
-
-void pantallaPrincipal(GtkWidget *wid, gpointer data)
+/*si jugador = 0 comienza cielo, si no comienza infierno*/
+void pantallaPrincipal(int jugador)
 {
-
-
-    GtkWidget *mainWindow;/*creamos primer pantalla*/
     mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);/*se inicializa*/
     g_signal_connect(mainWindow, "delete-event", G_CALLBACK(gtk_main_quit),NULL);/*evento para poder cerrarla*/
     gtk_window_set_title (GTK_WINDOW (mainWindow), "Gyges");/*le ponemos un titulo*/
@@ -35,26 +48,13 @@ void pantallaPrincipal(GtkWidget *wid, gpointer data)
     gtk_image_set_from_file (tablero,"Tablero.png");/*la cargamos del filename*/
     gtk_fixed_put (contenedor,tablero,1,40);/*lo ponemos en el contenedor en la posicion*/
 
-    GtkWidget *label2;/*creamos un label*/
-    label2 = gtk_label_new ("Turno del jugador:");
-    gtk_fixed_put (contenedor,label2,580,10);/*lo ponemos en el contenedor en la posicion*/
+    GtkWidget *lblJugadorActual;/*será el label que contiene el jugador actual*/
+    lblJugadorActual = gtk_label_new ("Turno del jugador:");
+    gtk_fixed_put (contenedor,lblJugadorActual,580,10);/*lo ponemos en el contenedor en la posicion*/
 
 
 
-    GtkWidget *buttonInteligencia = gtk_button_new_with_label ("Juega Inteligencia");
-    gtk_fixed_put (contenedor,buttonInteligencia,600,50);/*lo ponemos en el contenedor en la posicion*/
-
-    GtkWidget *buttonHumano = gtk_button_new_with_label ("Juega Humano");
-    gtk_fixed_put (contenedor,buttonHumano,600,100);/*lo ponemos en el contenedor en la posicion*/
-
-     GtkWidget *buttonSalir = gtk_button_new_with_label ("SALIR");
-    gtk_fixed_put (contenedor,buttonSalir,690,590);/*lo ponemos en el contenedor en la posicion*/
-
-    GtkWidget *entrada = gtk_entry_new();
-    gtk_fixed_put (contenedor,entrada,600,150);/*lo ponemos en el contenedor en la posicion*/
-
-    GtkWidget  *labelJugador;
-    if(((char*) data) == "1")/*juega cielo*/
+    if(jugador == 1)/*juega cielo*/
     {
         labelJugador = gtk_label_new ("Cielo");
     }
@@ -66,7 +66,7 @@ void pantallaPrincipal(GtkWidget *wid, gpointer data)
 
     gtk_widget_show (contenedor);/*mostramos el contenedor con todos los widgets*/
     gtk_widget_show_all (mainWindow);
-    gtk_widget_hide(PantallaPrimerTurno);/*la pantallla nunca es borrada, solo escondida"*/
+    juegoPrincipal(jugador);
 }
 
 void iniciarGtk(int argc, char *argv[])
@@ -77,34 +77,58 @@ void iniciarGtk(int argc, char *argv[])
       g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
 
 }
-void primerPantalla()
-/*Primer pantalla. Aquí se selecciona si comienza infierno o cielo.*/
-{
-    PantallaPrimerTurno = gtk_window_new(GTK_WINDOW_TOPLEVEL);/*se inicializa*/
-    g_signal_connect(PantallaPrimerTurno, "delete-event", G_CALLBACK(gtk_main_quit),NULL);/*evento para poder cerrarla*/
-    gtk_signal_connect (GTK_OBJECT (PantallaPrimerTurno), "destroy",GTK_SIGNAL_FUNC (gtk_exit), NULL);
-    gtk_widget_show(PantallaPrimerTurno);/*se muestra*/
-    gtk_window_set_title (GTK_WINDOW (PantallaPrimerTurno), "Escoja quien comienza");/*le ponemos un titulo*/
-    gtk_window_set_position (GTK_WINDOW (PantallaPrimerTurno), GTK_WIN_POS_CENTER);/* la ponemos en el centro*/
 
+void juegoPrincipal(int jugadorActual){
+    int control = 0;
+    char desicion[20],movimiento[20];
 
-    GtkWidget *buttonCielo = gtk_button_new_with_label ("Juega Cielo");
-    GtkWidget *buttonInfierno = gtk_button_new_with_label ("Juega Infierno");
-    /*asociamos un boton con una funcion*/
-    gtk_signal_connect (GTK_OBJECT (buttonCielo), "clicked",GTK_SIGNAL_FUNC (pantallaPrincipal), (gpointer) "1");
-    g_signal_connect (G_OBJECT (buttonInfierno), "clicked", G_CALLBACK (pantallaPrincipal), (gpointer) "2");
-
-    /*creamos un contenedor para los botones*/
-    GtkWidget *contenedor = gtk_vbox_new (TRUE, 6);
-    gtk_container_add (GTK_CONTAINER (PantallaPrimerTurno), contenedor);
-
-    /*metemos ambos botones en el contenedor*/
-    gtk_box_pack_start (GTK_BOX (contenedor), buttonCielo, TRUE, TRUE, 0);
-    gtk_box_pack_start (GTK_BOX (contenedor), buttonInfierno, TRUE, TRUE, 0);
-
-    gtk_widget_show_all (PantallaPrimerTurno);
-    gtk_main();
+    while (control == 0) {
+        while ( gtk_events_pending() ) gtk_main_iteration();
+        printf("\nSelecciona una opción: \n 0. Salir \n 1. Juega IA \n 2. Juega Persona\n");
+        scanf("%s", &desicion);
+        if(strcmp(desicion,"0") ==0){
+            printf("Nos vemos!");
+            control = 1;
+        }
+        else if(strcmp(desicion,"1") ==0){/*juega IA*/
+            siguienteTurno();
+        }
+        else if(strcmp(desicion,"2") ==0){/*juega Humano*/
+            printf("Ingresa el movimiento a realizar: \n");
+            scanf("%s", &movimiento);
+            /*ahora hay que ver si es un moviiento valido, si si se meuve, si no no pasa nada*/
+            /*muestraMensaje(mainWindow,"Movimiento ilegal. Juegue bien!");*/
+            siguienteTurno();
+        }
+        else
+        {
+            muestraMensaje(mainWindow,"Opcion incorrecta. Solo existen 3 opciones: 0 ,1 y 2.");
+        }
+    }
 }
+
+/*metodo que invierte los labels de la pantalla*/
+void siguienteTurno(){
+    if (strcmp(gtk_label_get_text (labelJugador), "Cielo")==0)/*le tocará jugar a infierno*/
+    {
+        gtk_label_set_text (labelJugador,"Infierno");
+    }
+    else{/*le tocará jugar a cielo*/
+        gtk_label_set_text (labelJugador,"Cielo");
+    }
+}
+
+/*muestra en pantalla un mensaje de error de jugada incorrecta*/
+/*recibe una pantalla padre, y un mensaje*/
+void muestraMensaje(GtkWidget *pantallaPadre, char* mensaje)
+{
+     GtkWidget *pantallita = NULL;
+     pantallita = gtk_message_dialog_new (GTK_WINDOW (pantallaPadre), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, mensaje);
+     gtk_window_set_position (GTK_WINDOW (pantallita), GTK_WIN_POS_CENTER);
+     gtk_dialog_run (GTK_DIALOG (pantallita));
+     gtk_widget_destroy (pantallita);
+}
+
 
 
 
